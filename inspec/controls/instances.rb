b74d8e83290db23e40b41227c08daccebc8ca73a
Martin Muller <martin.muller@alfresco.com>
2018-08-06 18:46:56 +0200

    repo-3694 repo was renamed

--- inspec/controls/instances.rb
+++ inspec/controls/instances.rb
@@ -1,5 +1,5 @@
-bastion = attribute('BastionSubStackName', default: '', description: 'K8s BastionSubStackName')
+bastion = attribute('BastionSubstackName', default: '', description: 'K8s BastionSubStackName')
 
-describe aws_ec2_instance(name: bastion + '-k8s-bastion') do
+describe aws_ec2_instance(name: bastion) do
   it { should be_running }
 end
