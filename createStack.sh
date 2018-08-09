737df9cb17f09defa862c1275fd0add0d014fbad
Martin Muller <martin.muller@alfresco.com>
2018-08-09 13:04:20 +0200

    lambda update not needed currently

--- createStack.sh
+++ createStack.sh
@@ -9,7 +9,6 @@ STACK_NAME=$1
 S3_BUCKET=$2
 
 aws s3 cp ./templates s3://$S3_BUCKET/acs --recursive
-aws s3 cp ./lambdas s3://$S3_BUCKET/acs/lambdas --recursive
 
 aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://templates/acs-deployment-master.yaml --capabilities CAPABILITY_IAM --parameters file://templates/acs-master-parameters.json --disable-rollback
 
