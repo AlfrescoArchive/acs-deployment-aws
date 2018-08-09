3c7877aaf1ec19d5ef87266fd6bf1e72a34e7003
Martin Muller <martin.muller@alfresco.com>
2018-08-09 15:58:11 +0200

    revert parameter file. add createStack.sh

--- createStack.sh
+++ createStack.sh
@@ -1,14 +1,67 @@
 #!/bin/bash
 
-if [[ $# -ne 2 ]] ; then
-    echo 'wrong number of parameters usage: ./createStack.sh <stack-name> <s3-bucket>'
+logInfo() {
+    echo "info::: $1"
+}
+
+logError() {
+    echo "error::: $1"
     exit 1
-fi
+}
 
-STACK_NAME=$1
-S3_BUCKET=$2
+printVarSummary() {
+    separator
+    echo "Variables Summary"
+    separator
+    for var in $VAR_LIST
+    do
+        echo "$var = ${!var}"
+    done
+    separator
+}
 
-aws s3 cp ./templates s3://$S3_BUCKET/acs --recursive
+usage() {
+  echo "Usage: aws_quickstart_createStack.sh <access_key_id> <secret_access_key> <cfn_template_body> <parameters_file> <stack_name> <aws_region>"
+  exit 1
+}
 
-aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://templates/acs-deployment-master.yaml --capabilities CAPABILITY_IAM --parameters file://templates/acs-master-parameters.json --disable-rollback
+if [ $# -lt 6 ]; then
+  usage
+else
+  export AWS_ACCESS_KEY_ID=$1
+  export AWS_SECRET_ACCESS_KEY=$2
+  TEMPLATE_BODY="$3"
+  PARAMETERS_FILE="$4"
+  STACK_NAME=$(echo -n $5 | tr / - | awk '{print tolower($0)}')
+  AWS_REGIOM=$6
 
+  CAPABILITIES=CAPABILITY_IAM
+  STACK_CREATION_TIMEOUT=600
+
+  # Some logging
+  printVarSummary
+
+  # Validate the template
+  separator
+  logInfo "Validate the Cloudformation template"
+  aws cloudformation validate-template --template-body $TEMPLATE_BODY --region $6
+
+  # Create the stack
+  separator
+  aws cloudformation create-stack \
+      --stack-name $STACK_NAME \
+      --template-body $TEMPLATE_BODY \
+      --parameters file://$PARAMETERS_FILE \
+      --region $6 \
+      --capabilities $CAPABILITIES
+      # --disable-rollback
+
+  if [ $? -ne 0 ]; then
+    logError "Stack creation failed."
+  fi
+
+  # Wait until stack complete
+  aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
+
+  # Show stack
+  aws cloudformation describe-stacks --stack-name $STACK_NAME
