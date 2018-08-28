f9672cf00b9cca010c3c37d6e3f62ebd43b471b0
Martin Muller <martin.muller@alfresco.com>
2018-08-28 15:15:16 +0100

    Revert "repo-3797 extend endpoint.sh for defining sleeping time"
    This reverts commit 1b86c061750888308821a2d84bcd6b0c1d029ff1.

--- inspec/controls/endpoints.sh
+++ inspec/controls/endpoints.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ $# -gt 2 ]
-  then
-    echo "usage: endpoint.sh <url> <sleeping in second>"
-fi
-
 base_url=$1
 
 echo "Share Endpoint is: $base_url/share/page"
@@ -19,18 +14,13 @@ RETRY=0
 MAX_RETRY=20
 SLEEP=30
 
-if [ $# -eq 2 ]
-  then
-    SLEEP=$2
-fi
-
 while [ "$STATUS" != "401" ]; do
   if [ "$RETRY" -eq "$MAX_RETRY" ]; then
     echo "DNS resolution timed out"
     echo "DNS is not available - exit"
     exit 1
   fi
-  echo "DNS is not ready yet. Retry:" $(($MAX_RETRY - $RETRY)) "  Sleeping..."
+  echo "DNS is not ready yet.  Sleeping..."
   sleep $SLEEP
   STATUS=$(curl -s -o /dev/null -w ''%{http_code}'' $acs_url)
   RETRY=`expr $RETRY + 1`
