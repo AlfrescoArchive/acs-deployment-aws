d96086d3ca6bbf50dededfce10a1c173d7817616
Alex Mukha <alex.mukha@alfresco.com>
2018-12-11 16:55:10 +0000

    REPO-4089 Update to latest ACS

--- scripts/helmAcs.sh
+++ scripts/helmAcs.sh
@@ -163,7 +163,7 @@ repository:
   adminPassword: \"$ALFRESCO_PASSWORD\"
   image:
     repository: \"alfresco/alfresco-content-repository-aws\"
-    tag: \"6.1.0-EA3\"
+    tag: \"6.1.0-RC3\"
   replicaCount: $REPO_PODS
   environment:
     JAVA_OPTS: \" -Dopencmis.server.override=true -Dopencmis.server.value=https://$EXTERNAL_NAME -Dalfresco.restApi.basicAuthScheme=true -Dsolr.base.url=/solr -Dsolr.secureComms=none -Dindex.subsystem.name=solr6 -Dalfresco.cluster.enabled=true -Ddeployment.method=HELM_CHART -Xms2000M -Xmx2000M\"
@@ -237,7 +237,7 @@ share:
     initialDelaySeconds: 420
 registryPullSecrets: quay-registry-secret" > acs_install_values.yaml
 
-    helm install alfresco-incubator/alfresco-content-services --version 1.1.6 -f acs_install_values.yaml --name $ACS_RELEASE --namespace=$DESIREDNAMESPACE
+    helm install alfresco-incubator/alfresco-content-services --version 1.1.7 -f acs_install_values.yaml --name $ACS_RELEASE --namespace=$DESIREDNAMESPACE
 
   fi
 
@@ -259,7 +259,7 @@ registryPullSecrets: quay-registry-secret" > acs_install_values.yaml
       --set s3connector.secrets.encryption=kms \
       --set s3connector.secrets.awsKmsKeyId="$S3BUCKET_KMS_ALIAS" \
       --set repository.environment.JAVA_OPTS=" -Dopencmis.server.override=true -Dopencmis.server.value=https://$EXTERNAL_NAME -Dalfresco.restApi.basicAuthScheme=true -Dsolr.base.url=/solr -Dsolr.secureComms=none -Dindex.subsystem.name=solr6 -Dalfresco.cluster.enabled=true -Ddeployment.method=HELM_CHART -Xms2000M -Xmx2000M" \
-      --set repository.image.tag="6.1.0-EA3" \
+      --set repository.image.tag="6.1.0-RC3" \
       --set repository.replicaCount="$REPO_PODS" \
       --namespace=$DESIREDNAMESPACE
   fi
