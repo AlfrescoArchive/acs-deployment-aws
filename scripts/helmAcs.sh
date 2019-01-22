44c3a34e28792eb2ab6159b866f510941701e6fa
Martin Muller <martin.muller@alfresco.com>
2019-01-22 12:20:45 +0000

    REPO-4027 try reuse values for keeping the pvc

--- scripts/helmAcs.sh
+++ scripts/helmAcs.sh
@@ -264,8 +264,8 @@ registryPullSecrets: quay-registry-secret" > $VALUES_FILE
 
   if [ "$UPGRADE" = "true" ]; then
     echo Upgrading Alfresco Content Services helm chart...
-    helm upgrade $ACS_RELEASE alfresco-stable/alfresco-content-services --version $CHART_VERSION -f $VALUES_FILE \
-     --install --force --namespace=$DESIREDNAMESPACE
+    helm upgrade $ACS_RELEASE alfresco-stable/alfresco-content-services --version $CHART_VERSION --reuse-values -f $VALUES_FILE \
+     --install --namespace=$DESIREDNAMESPACE
   fi
 
   STATUS=$(helm ls $ACS_RELEASE | grep $ACS_RELEASE | awk '{print $8}')
